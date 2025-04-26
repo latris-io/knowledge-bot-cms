477158ecb9cef3712225aef0ee7f886b40941ab7
latris-io <90663113+latris-io@users.noreply.github.com>
2025-04-26 18:37:28 -0500

    Update server.js

--- config/server.js
+++ config/server.js
@@ -1,10 +1,8 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
   port: env.int('PORT', 1337),
+  url: env('RENDER_EXTERNAL_URL'), // Optional: use Render URL in production
   app: {
     keys: env.array('APP_KEYS'),
-  },
-  webhooks: {
-    populateRelations: env.bool('WEBHOOKS_POPULATE_RELATIONS', false),
   },
 });
